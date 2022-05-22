132469a42a7571ec8ce364e8dd69af6ee260d635
PC <PC@DESKTOP-84U7664>
2022-05-22 14:48:25 +0700

    Add project files.

---
+++ ScanWeb/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace ScanWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Close = new System.Windows.Forms.Button();
+            this.XssCheckBox = new System.Windows.Forms.CheckBox();
+            this.SqlCheckBox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(756, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(906, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(121, 156);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(860, 139);
+            this.listBox2.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1098, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Close);
+            this.panel1.Controls.Add(this.XssCheckBox);
+            this.panel1.Controls.Add(this.SqlCheckBox);
+            this.panel1.Location = new System.Drawing.Point(410, 119);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 176);
+            this.panel1.TabIndex = 6;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(111, 142);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 2;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Visible = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // XssCheckBox
+            // 
+            this.XssCheckBox.AutoSize = true;
+            this.XssCheckBox.Checked = true;
+            this.XssCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.XssCheckBox.Location = new System.Drawing.Point(155, 55);
+            this.XssCheckBox.Name = "XssCheckBox";
+            this.XssCheckBox.Size = new System.Drawing.Size(92, 19);
+            this.XssCheckBox.TabIndex = 1;
+            this.XssCheckBox.Text = "Xss Injection";
+            this.XssCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SqlCheckBox
+            // 
+            this.SqlCheckBox.AutoSize = true;
+            this.SqlCheckBox.Checked = true;
+            this.SqlCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SqlCheckBox.Location = new System.Drawing.Point(53, 55);
+            this.SqlCheckBox.Name = "SqlCheckBox";
+            this.SqlCheckBox.Size = new System.Drawing.Size(91, 19);
+            this.SqlCheckBox.TabIndex = 0;
+            this.SqlCheckBox.Text = "Sql Injection";
+            this.SqlCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.CheckBox XssCheckBox;
+        private System.Windows.Forms.CheckBox SqlCheckBox;
+    }
+}
+
---
+++ ScanWeb/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace ScanWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //using(OpenFileDialog dialog = new OpenFileDialog())
+            //{
+            //    dialog.Multiselect = false;
+            //    dialog.InitialDirectory = @"E:\";
+            //    dialog.Filter = "Text files(*.txt)|*.txt";
+            //    if(dialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        textBox1.Text = File.ReadAllText(dialog.FileName).ToString();
+            //    }    
+            //}
+
+
+            //if (UrlIsValid(textBox1.Text) == true)
+            //    MessageBox.Show("The URL is Valid");
+            //else
+            //    MessageBox.Show("The URL is not Valid");
+            listBox2.Items.Clear();
+            GetURL(textBox1.Text);
+        }
+        //public bool UrlIsValid(string url)
+        //{
+        //    try
+        //    {
+        //        HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
+        //        request.Timeout = 5000;
+        //        request.Method = "HEAD";
+
+        //        using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+        //        {
+        //            int statusCode = (int)response.StatusCode;
+        //            if (statusCode >= 100 && statusCode < 400)
+        //            {
+        //                return true;
+        //            }
+        //            else if (statusCode >= 500 && statusCode <= 510)
+        //            {
+        //                return false;
+        //            }
+        //        }
+        //    }
+        //    catch (WebException ex)
+        //    {
+        //        if (ex.Status == WebExceptionStatus.ProtocolError) //400 errors
+        //        {
+        //            return false;
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //    return false;
+        //}
+
+        public void ScanXss(string parm, string _url)
+        {
+            string _xssUrl = _url.Replace(parm, parm + "ra<xss>it");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_xssUrl);
+            request.Method = "GET";
+            string _xss = string.Empty;
+            using (StreamReader lectura = new StreamReader(request.GetResponse().GetResponseStream())) _xss = lectura.ReadToEnd();
+            if (_xss.Contains("<xss>"))
+            {
+                listBox2.Items.Add(parm + "\t" + "Have a xss Vulnerabilities");
+            }
+            else
+            {
+                listBox2.Items.Add(parm + "\t" + "Don't have a xss Vulnerabilities");
+            }
+        }
+
+        public void ScanSql(string parm, string _url)
+        {
+            string _sqlUrl = _url.Replace(parm, parm + "ra'it");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_sqlUrl);
+            request.Method = "GET";
+            string _sql = string.Empty;
+            using (StreamReader lectura = new StreamReader(request.GetResponse().GetResponseStream())) _sql = lectura.ReadToEnd();
+            if (_sql.Contains("error in your SQL syntax"))
+            {
+                listBox2.Items.Add(parm + "\t" + "Have a Sql Injection Vulnerability");
+            }
+            else
+            {
+                listBox2.Items.Add(parm + "\t" + "Don't have a Sql Injection Vulnerability");
+            }
+        }
+
+        public void GetURL(string txt)
+        {
+            List<string> _listUrl = new List<string>();
+            HtmlWeb hw = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = hw.Load(txt);
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                _listUrl.Add(att.Value);
+            }
+            foreach (var item in _listUrl)
+            {
+                string _url = item.ToString();
+                int index = _url.IndexOf('?');
+                string[] parms = _url.Remove(0, index + 1).Split('&');
+
+                foreach (string parm in parms)
+                {
+                    try 
+                    {
+                        if (XssCheckBox.Checked == true && SqlCheckBox.Checked == false)
+                        {
+                            ScanXss(parm, _url);
+                        }else if(SqlCheckBox.Checked == true && XssCheckBox.Checked == false)
+                        {
+                            ScanSql(parm, _url);
+                        }else if(XssCheckBox.Checked == true && SqlCheckBox.Checked == true)
+                        {
+                            ScanSql(parm, _url);
+                            ScanXss(parm, _url);
+                        }    
+                    } 
+                    catch (Exception ex) { }
+                    
+                }
+
+            }
+        }
+
+        private void optionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            if(SqlCheckBox.Checked == false && XssCheckBox.Checked == false)
+            {
+                button2.Enabled = false;
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            panel1.Visible = false;
+            Close.Visible = false;
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            Close.Visible = true;
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
